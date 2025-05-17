c9a3a21cc85a307abead099437c977bf2215fea4
Pham Minh <phamvanminh1723@gmail.com>
2025-05-17 23:11:52 +0700

    change main class name

--- backend/src/main/java/com/datingapp/DatingAppApplication.java
+++ backend/src/main/java/com/datingapp/DatingAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmouraDatingAppApplication {
+public class DatingAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AmouraDatingAppApplication.class, args);
+		SpringApplication.run(DatingAppApplication.class, args);
 	}
 
 }
