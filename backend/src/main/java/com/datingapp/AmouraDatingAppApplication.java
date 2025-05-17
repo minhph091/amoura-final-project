88fd903305c45f5985195f0df1a507af95f06f9f
Pham Minh <phamvanminh1723@gmail.com>
2025-05-17 22:31:31 +0700

    cấu hình dependencies

---
+++ backend/src/main/java/com/datingapp/AmouraDatingAppApplication.java
@@ -0,0 +1,13 @@
+package com.datingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmouraDatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmouraDatingAppApplication.class, args);
+	}
+
+}
