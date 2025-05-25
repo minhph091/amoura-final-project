bb579f52d7efb6f269a917527d7aec37f38f47e7
sondn04 <sondang10082004@gmail.com>
2025-05-26 00:34:29 +0700

    Cập nhật hiển thị icon và badge trong navigation bar

--- mobile-app/lib/presentation/main_navigator/widgets/gradient_icon.dart
+++ mobile-app/lib/presentation/main_navigator/widgets/gradient_icon.dart
@@ -1,16 +1,16 @@
-// lib/presentation/main_navigator/widgets/gradient_icon_3d.dart
+// lib/presentation/main_navigator/widgets/gradient_icon.dart
 // Gradient and 3D-style icon widget for navigation or action buttons.
 
 import 'package:flutter/material.dart';
 
-class GradientIcon3D extends StatelessWidget {
+class GradientIcon extends StatelessWidget {
   final IconData icon;
   final double size;
   final Gradient gradient;
   final bool withShadow;
   final double elevation;
 
-  const GradientIcon3D({
+  const GradientIcon({
     super.key,
     required this.icon,
     required this.size,
@@ -21,14 +21,29 @@ class GradientIcon3D extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: withShadow ? elevation : 0.0,
-      color: Colors.transparent,
-      shape: const CircleBorder(),
-      child: ShaderMask(
-        blendMode: BlendMode.srcIn,
-        shaderCallback: (Rect bounds) => gradient.createShader(bounds),
-        child: Icon(icon, size: size),
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: withShadow
+            ? [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.2),
+            blurRadius: 6,
+            spreadRadius: 1,
+          ),
+        ]
+            : [],
+      ),
+      child: Material(
+        elevation: withShadow ? elevation : 0.0,
+        color: Colors.transparent,
+        shape: const CircleBorder(),
+        child: ShaderMask(
+          blendMode: BlendMode.srcIn,
+          shaderCallback: (Rect bounds) => gradient.createShader(bounds),
+          child: Icon(icon, size: size),
+        ),
       ),
     );
   }
