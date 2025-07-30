b1cbc9562049c8bad4acc34f91123abef1a91f74
LeBaoMy <lebaomy584@gmail.com>
2025-07-30 16:04:22 +0700

    test: endpoint admin

--- backend/src/test/java/com/amoura/module/admin/api/AdminControllerTests.java
+++ backend/src/test/java/com/amoura/module/admin/api/AdminControllerTests.java
@@ -1,6 +1,7 @@
 package com.amoura.module.admin.api;
 
 import com.amoura.common.AdminLoginAndGetToken;
+import com.amoura.common.LoginAndGetToken;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -8,13 +9,16 @@ import org.junit.jupiter.api.Test;
 
 public class AdminControllerTests {
 
-    static String jwtToken;
+    static String adminToken;
+    static String userToken;
 
     @BeforeAll
     static void setup() {
         RestAssured.baseURI = "http://localhost/api";
         RestAssured.port = 8080;
-        jwtToken = AdminLoginAndGetToken.execute();
+
+        adminToken = AdminLoginAndGetToken.execute();
+        userToken = LoginAndGetToken.execute();
     }
 
     @Test
@@ -22,11 +26,25 @@ public class AdminControllerTests {
     void getAdminDashboard_WithValidAdminToken() {
         RestAssured
                 .given()
-                .header("Authorization", "Bearer " + jwtToken)
+                .header("Authorization", "Bearer " + adminToken)
                 .when()
                 .get("/admin/dashboard")
                 .then()
                 .log().all()
                 .statusCode(200);
     }
+
+    @Test
+    @DisplayName("Lấy admin dashboard - Token user thường ")
+    void getAdminDashboard_WithUserToken() {
+        RestAssured
+                .given()
+                .header("Authorization", "Bearer " + userToken)
+                .when()
+                .get("/admin/dashboard")
+                .then()
+                .log().all()
+                .statusCode(403);
+    }
+
 }
